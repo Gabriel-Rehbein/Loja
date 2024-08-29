47a1b9c8eb214f935bfe2f7c5ac6598da81cd5ba
Gabriel-Rehbein <152233295+Gabriel-Rehbein@users.noreply.github.com>
2024-08-29 18:57:18 -0300

    .

---
+++ Loja.py
@@ -0,0 +1,29 @@
+class Mensagem:
+    def __init__(self):
+        print("\nOlá, seja bem-vindo à --> Loja Compre Já! <--\n")
+
+class Estoque:
+    def __init__(self, produtos_ja_adicionados):
+        Produtos_ja_adicionados = []
+
+    def imprimir_estoque
+        
+
+class Produtos:
+    def pergunta_adicionar_produto(self):
+        produto = input("Qual produto você deseja adicionar: ")
+        quantidade = int(input("Qual é a quantidade desse produto: "))
+        return produto, quantidade
+
+
+class Linha:
+    def __init__(self):
+        print("-x-x-x-x-x-x-x-x-x-x--x-x-x-x")
+
+
+linha1 = Linha()
+mensagem = Mensagem()
+linha2 = Linha()
+
+produtos = Produtos()  
+produto_adicionado, quantidade = produtos.pergunta_adicionar_produto() 
